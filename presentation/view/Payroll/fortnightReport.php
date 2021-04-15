b837657e836f401d19334334cfb630d55ed9856b
Jordan Méndez <jordanemendezt@gmail.com>
2021-04-15 16:27:57 -0600

    Readjustment fortnight report

--- presentation/view/Payroll/fortnightReport.php
+++ presentation/view/Payroll/fortnightReport.php
@@ -32,9 +32,13 @@ $currencyPath = 'presentation/public/img/colon.png';
             .border {
                 border: 1px solid #8c8c8c;
             }
-            
+
             .content-loader tr td {
                 white-space: nowrap;
+            }
+            
+            .small-p {
+                font-size: 11px;
             }
         </style>
     </head>
@@ -54,8 +58,8 @@ $currencyPath = 'presentation/public/img/colon.png';
                 </tr>
             </table>
         </div>
-        
-        
+
+
         <div>
             <table>
                 <tr>
@@ -67,165 +71,163 @@ $currencyPath = 'presentation/public/img/colon.png';
                 </tr>
             </table>
         </div>
-        
-        
-        
+
+
+
         <div class="body">
             <div style="width: 250px;">
                 <table>
                     <tr>
-                        <th style="width: 55%">Quincena</th>
+                        <th class="border" style="width: 55%">Quincena</th>
                         <td style="text-align: center; width: 45%;" class="border"><?= $data[0]['fortnight'] ?></td>
                     </tr>    
 
                     <tr>
-                        <th style="width: 55%">Año</th>
+                        <th class="border" style="width: 55%">Año</th>
                         <td style="text-align: center; width: 45%;" class="border"><?= $data[0]['year'] ?></td>
                     </tr> 
 
                     <tr>
-                        <th style="width: 55%">Localidad</th>
+                        <th class="border" style="width: 55%">Localidad</th>
                         <td style="text-align: center; width: 45%;" class="border"><?= $data[0]['location'] ?></td>
                     </tr> 
                 </table>
             </div>
-            
+
             <br>
-            
+
             <div>
                 <table>
-                    <thead>    
-                        <tr>
-                            <th style="text-align:">Nombre del Empleado</th>
-                            <th style="text-align:">Ordinario</th>
-                            <th style="text-align:">Vacación</th>
-                            <th style="text-align:">Extra</th>
-                            <th style="text-align:">Doble</th>
-                            <th style="text-align:">Recargo</th>
-                            <th style="text-align:">Tot. Devengado</th>
-                            <th style="text-align:">Seguro Social</th>
-                            <th style="text-align:">Imp. Renta</th>
-                            <th style="text-align:">Total Deducciones</th>
-                            <th style="text-align:">Neto por Pagar</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php 
-                        $totalOrdinary = 0;
-                        $totalVacation = 0;
-                        $totalExtra = 0;
-                        $totalDouble = 0;
-                        $totalSurcharges = 0;
-                        $totalAccrued = 0;
-                        $totalCCSS = 0;
-                        $totalIncome = 0;
-                        $totalDeduction = 0;
-                        $totalDisabilities = 0;
-                        $totalNet = 0;
-                        foreach ($data as $value) {
-                            ?> 
-                            <tr>
-                                <td style="text-align:" class="border"><?= $value['completeName'] ?></td>
-                                <td style="text-align:" class="border">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($value['ordinary'], 2, '.', ' '); ?>   
-                                </td>
-                                <td style="text-align:" class="border">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($value['vacationAmount'], 2, '.', ' '); ?>
-                                </td>
-                                <td style="text-align:" class="border">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($value['extra'], 2, '.', ' '); ?>
-                                </td>
-                                <td style="text-align:" class="border">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($value['double'], 2, '.', ' '); ?>
-                                </td>
-                                <td style="text-align:" class="border">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($value['surcharges'], 2, '.', ' '); ?>
-                                </td>
-                                <td style="text-align:" class="border">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($value['gross'], 2, '.', ' '); ?>
-                                </td>
-                                <td style="text-align:" class="border">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($value['workerCCSS'], 2, '.', ' '); ?>
-                                </td>
-                                <td style="text-align:" class="border">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($value['incomeTax'], 2, '.', ' '); ?>
-                                </td>
-                                <td style="text-align:" class="border">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($value['deductionsTotal'], 2, '.', ' '); ?>
-                                </td>
-                                <td style="text-align:" class="border">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($value['net'], 2, '.', ' '); ?>
-                                </td>
+                    <tr>
+                        <th class="border small-p" style="width: 20%">Nombre del Empleado</th>
+                        <th class="border small-p" style="width: 8%">Ordinario</th>
+                        <th class="border small-p" style="width: 8%">Vacación</th>
+                        <th class="border small-p" style="width: 8%">Extra</th>
+                        <th class="border small-p" style="width: 8%">Doble</th>
+                        <th class="border small-p" style="width: 8%">Recargo</th>
+                        <th class="border small-p" style="width: 8%">Tot. Devengado</th>
+                        <th class="border small-p" style="width: 8%">Seguro Social</th>
+                        <th class="border small-p" style="width: 8%">Imp. Renta</th>
+                        <th class="border small-p" style="width: 8%">Total Deducciones</th>
+                        <th class="border small-p" style="width: 8%">Neto por Pagar</th>
+                    </tr>
 
-                            </tr>
-                            <?php   
-                            $totalOrdinary += $value['ordinary'];
-                            $totalVacation += $value['vacationAmount'];
-                            $totalExtra += $value['extra'];
-                            $totalDouble += $value['double'];
-                            $totalSurcharges += $value['surcharges'];
-                            $totalAccrued += $value['gross'];
-                            $totalCCSS += $value['workerCCSS'];
-                            $totalIncome += $value['incomeTax'];
-                            $totalDeduction += $value['deductionsTotal'];
-                            $totalDisabilities += $value['ccssAmount'] + $value['insAmount'];
-                            $totalNet += $value['net'];
-                        }
-                        ?>
-                            <tr>
-                                <th style="text-align: center;">Total general</th>
-                                <th style="text-align: center;">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($totalOrdinary, 2, '.', ' '); ?>
-                                </th>
-                                <th style="text-align: center;">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($totalVacation, 2, '.', ' '); ?>
-                                </th>
-                                <th style="text-align: center;">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($totalExtra, 2, '.', ' '); ?>
-                                </th>
-                                <th style="text-align: center;">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($totalDouble, 2, '.', ' '); ?>
-                                </th>
-                                <th style="text-align: center;">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($totalSurcharges, 2, '.', ' '); ?>
-                                </th>
-                                <th style="text-align: center;">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($totalAccrued, 2, '.', ' '); ?>
-                                </th>
-                                <th style="text-align: center;">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($totalCCSS, 2, '.', ' '); ?>
-                                </th>
-                                <th style="text-align: center;">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($totalIncome, 2, '.', ' '); ?>
-                                </th>
-                                <th style="text-align: center;">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($totalDeduction, 2, '.', ' '); ?>
-                                </th>
-                                <th style="text-align: center;">
-                                    <img src="<?= $currencyPath ?>" height="12">
-                                    <?= number_format($totalNet, 2, '.', ' '); ?>
-                                </th>
-                            </tr>
-                    </tbody>
+                    <?php
+                    $totalOrdinary = 0;
+                    $totalVacation = 0;
+                    $totalExtra = 0;
+                    $totalDouble = 0;
+                    $totalSurcharges = 0;
+                    $totalAccrued = 0;
+                    $totalCCSS = 0;
+                    $totalIncome = 0;
+                    $totalDeduction = 0;
+                    $totalDisabilities = 0;
+                    $totalNet = 0;
+                    foreach ($data as $value) {
+                        ?> 
+                        <tr>
+                            <td style="width: 20%" class="border small-p"><?= $value['completeName'] ?></td>
+                            <td style="width: 8%" class="border small-p">
+                                <img src="<?= $currencyPath ?>" height="12">
+                                <?= number_format($value['ordinary'], 2, '.', ' '); ?>   
+                            </td>
+                            <td style="width: 8%" class="border small-p">
+                                <img src="<?= $currencyPath ?>" height="12">
+                                <?= number_format($value['vacationAmount'], 2, '.', ' '); ?>
+                            </td>
+                            <td style="width: 8%" class="border small-p">
+                                <img src="<?= $currencyPath ?>" height="12">
+                                <?= number_format($value['extra'], 2, '.', ' '); ?>
+                            </td>
+                            <td style="width: 8%" class="border small-p">
+                                <img src="<?= $currencyPath ?>" height="12">
+                                <?= number_format($value['double'], 2, '.', ' '); ?>
+                            </td>
+                            <td style="width: 8%" class="border small-p">
+                                <img src="<?= $currencyPath ?>" height="12">
+                                <?= number_format($value['surcharges'], 2, '.', ' '); ?>
+                            </td>
+                            <td style="width: 8%" class="border small-p">
+                                <img src="<?= $currencyPath ?>" height="12">
+                                <?= number_format($value['gross'], 2, '.', ' '); ?>
+                            </td>
+                            <td style="width: 8%" class="border small-p">
+                                <img src="<?= $currencyPath ?>" height="12">
+                                <?= number_format($value['workerCCSS'], 2, '.', ' '); ?>
+                            </td>
+                            <td style="width: 8%" class="border small-p">
+                                <img src="<?= $currencyPath ?>" height="12">
+                                <?= number_format($value['incomeTax'], 2, '.', ' '); ?>
+                            </td>
+                            <td style="width: 8%" class="border small-p">
+                                <img src="<?= $currencyPath ?>" height="12">
+                                <?= number_format($value['deductionsTotal'], 2, '.', ' '); ?>
+                            </td>
+                            <td style="width: 8%" class="border small-p">
+                                <img src="<?= $currencyPath ?>" height="12">
+                                <?= number_format($value['net'], 2, '.', ' '); ?>
+                            </td>
+
+                        </tr>
+                        <?php
+                        $totalOrdinary += $value['ordinary'];
+                        $totalVacation += $value['vacationAmount'];
+                        $totalExtra += $value['extra'];
+                        $totalDouble += $value['double'];
+                        $totalSurcharges += $value['surcharges'];
+                        $totalAccrued += $value['gross'];
+                        $totalCCSS += $value['workerCCSS'];
+                        $totalIncome += $value['incomeTax'];
+                        $totalDeduction += $value['deductionsTotal'];
+                        $totalDisabilities += $value['ccssAmount'] + $value['insAmount'];
+                        $totalNet += $value['net'];
+                    }
+                    ?>
+
+                    <tr>
+                        <th style="width: 20%" class="border small-p">Total general</th>
+                        <th style="width: 8%" class="border small-p">
+                            <img src="<?= $currencyPath ?>" height="12">
+                            <?= number_format($totalOrdinary, 2, '.', ' '); ?>
+                        </th>
+                        <th style="width: 8%" class="border small-p">
+                            <img src="<?= $currencyPath ?>" height="12">
+                            <?= number_format($totalVacation, 2, '.', ' '); ?>
+                        </th>
+                        <th style="width: 8%" class="border small-p">
+                            <img src="<?= $currencyPath ?>" height="12">
+                            <?= number_format($totalExtra, 2, '.', ' '); ?>
+                        </th>
+                        <th style="width: 8%" class="border small-p">
+                            <img src="<?= $currencyPath ?>" height="12">
+                            <?= number_format($totalDouble, 2, '.', ' '); ?>
+                        </th>
+                        <th style="width: 8%" class="border small-p">
+                            <img src="<?= $currencyPath ?>" height="12">
+                            <?= number_format($totalSurcharges, 2, '.', ' '); ?>
+                        </th>
+                        <th style="width: 8%" class="border small-p">
+                            <img src="<?= $currencyPath ?>" height="12">
+                            <?= number_format($totalAccrued, 2, '.', ' '); ?>
+                        </th>
+                        <th style="width: 8%" class="border small-p">
+                            <img src="<?= $currencyPath ?>" height="12">
+                            <?= number_format($totalCCSS, 2, '.', ' '); ?>
+                        </th>
+                        <th style="width: 8%" class="border small-p">
+                            <img src="<?= $currencyPath ?>" height="12">
+                            <?= number_format($totalIncome, 2, '.', ' '); ?>
+                        </th>
+                        <th style="width: 8%" class="border small-p">
+                            <img src="<?= $currencyPath ?>" height="12">
+                            <?= number_format($totalDeduction, 2, '.', ' '); ?>
+                        </th>
+                        <th style="width: 8%" class="border small-p">
+                            <img src="<?= $currencyPath ?>" height="12">
+                            <?= number_format($totalNet, 2, '.', ' '); ?>
+                        </th>
+                    </tr>
                 </table>
             </div>
         </div>
